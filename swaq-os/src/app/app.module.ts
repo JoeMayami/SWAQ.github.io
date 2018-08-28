e8a6d22bd05859355994ecc4ac2c4d3ccf3017dd
JoeMayami <36084817+JoeMayami@users.noreply.github.com>
2018-08-28 01:58:30 +0100

    new component added, "Splash-Screen Component"

--- swaq-os/src/app/app.module.ts
+++ swaq-os/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent
   ],
   imports: [
     BrowserModule,
