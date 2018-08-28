ddfd4ca98fc20e8eaab0d918126aa4dfc238cd51
JoeMayami <36084817+JoeMayami@users.noreply.github.com>
2018-08-28 20:13:29 +0100

    working version

--- swaq-os/src/app/custom-module.module.ts
+++ swaq-os/src/app/custom-module.module.ts
@@ -1,11 +1,16 @@
 
 import {MatButtonModule, MatCheckboxModule, MatProgressSpinnerModule} from '@angular/material';
 import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatProgressSpinnerModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatProgressSpinnerModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatProgressSpinnerModule, MatInputModule, 
+    MatGridListModule, MatIconModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatProgressSpinnerModule, MatInputModule, 
+    MatGridListModule, MatIconModule],
 })
 export class CustomMaterialModule {
     // ...
