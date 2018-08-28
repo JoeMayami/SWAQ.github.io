22dbd4a72a7207040ef5fa22ebfbf8bd25deede7
JoeMayami <36084817+JoeMayami@users.noreply.github.com>
2018-08-28 06:45:35 +0100

    added cardview

--- swaq-os/src/app/app.module.ts
+++ swaq-os/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { CustomMaterialModule } from './custom-module.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { SplashScreenComponent } from './splash-screen/splash-screen.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    CustomMaterialModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- swaq-os/src/app/splash-screen/splash-screen.component.ts
+++ swaq-os/src/app/splash-screen/splash-screen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-splash-screen',
   templateUrl: './splash-screen.component.html',
