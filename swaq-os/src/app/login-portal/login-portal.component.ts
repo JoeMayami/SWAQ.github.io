6f9421e78a4c0d232eac8da5cf8fddc9f325299a
JoeMayami <36084817+JoeMayami@users.noreply.github.com>
2018-08-28 20:04:39 +0100

    fixing merge

--- swaq-os/src/app/login-portal/login-portal.component.ts
+++ swaq-os/src/app/login-portal/login-portal.component.ts
@@ -2,28 +2,12 @@ import { CustomMaterialModule } from './../custom-module.module';
 import { Component, OnInit, NgModule } from '@angular/core';
 
 
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> eb01b2cf0905bd9bfad3ac560d70cdfd0f8dee24
-
 @Component({
   selector: 'app-login-portal',
   templateUrl: './login-portal.component.html',
   styleUrls: ['./login-portal.component.css']
 })
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-})
- 
-=======
->>>>>>> eb01b2cf0905bd9bfad3ac560d70cdfd0f8dee24
-=======
->>>>>>> eb01b2cf0905bd9bfad3ac560d70cdfd0f8dee24
 export class LoginPortalComponent implements OnInit {
 
   onClick(event){
