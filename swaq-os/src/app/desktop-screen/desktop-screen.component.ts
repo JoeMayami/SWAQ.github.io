8b25789236442f05381882d89e1bedf17808cead
JoeMayami <36084817+JoeMayami@users.noreply.github.com>
2018-08-29 10:06:28 +0100

    new file

--- swaq-os/src/app/desktop-screen/desktop-screen.component.ts
+++ swaq-os/src/app/desktop-screen/desktop-screen.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomMaterialModule } from '../custom-module.module';
+import { trigger, transition, style, animate, state } from '@angular/animations';
+import { fadeIn } from './animate';
 
 @Component({
   selector: 'app-desktop-screen',
   templateUrl: './desktop-screen.component.html',
-  styleUrls: ['./desktop-screen.component.css']
+  styleUrls: ['./desktop-screen.component.css'],
+  animations:[
+    fadeIn
+  ]
 })
 export class DesktopScreenComponent implements OnInit {
 
---
+++ swaq-os/src/app/desktop-screen/profileImage.ts
@@ -0,0 +1,7 @@
+
+export class ProfileImage {
+
+    constructor () {
+        
+    }
+}
