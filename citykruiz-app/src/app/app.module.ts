25a2c8afe882a74f878ca6f583abdb5610ab2340
designerseyi <40296336+designerseyi@users.noreply.github.com>
2018-08-29 21:47:27 +0000

    files

--- citykruiz-app/src/app/app.module.ts
+++ citykruiz-app/src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+  import { BrowserModule } from '@angular/platform-browser';
+  import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginPortalComponent } from './login-portal/login-portal.component';
+import { SigupPortalComponent } from './sigup-portal/sigup-portal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPortalComponent,
+    SigupPortalComponent
   ],
   imports: [
     BrowserModule
---
+++ citykruiz-app/src/app/sigup-portal/sigup-portal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sigup-portal',
+  templateUrl: './sigup-portal.component.html',
+  styleUrls: ['./sigup-portal.component.css']
+})
+export class SigupPortalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
