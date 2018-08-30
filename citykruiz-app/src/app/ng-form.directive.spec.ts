bdf7e4f2fb8df586bf2e98ed2f95a6f6fee805d2
designerseyi <40296336+designerseyi@users.noreply.github.com>
2018-08-30 07:11:18 +0000

    ghgh

--- citykruiz-app/src/app/ng-form.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NgFormDirective } from './ng-form.directive';
-
-describe('NgFormDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NgFormDirective();
-    expect(directive).toBeTruthy();
-  });
-});
